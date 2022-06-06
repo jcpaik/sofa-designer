a53c9b78980c874a93d93e976b3c3e360855c43e
Jineon Baek <jcpaik@Jineons-MacBook-Air.local>
2022-06-06 10:16:36 -0400

    Parse integer

--- bin/parse.cc
+++ bin/parse.cc
@@ -1,5 +1,7 @@
 #include "parse.h"
 
+#include <exception>
+
 Parser::Parser(const SofaContext &ctx) : ctx(ctx) {}
 
 /*
@@ -13,3 +15,34 @@ LinearInequality Parser::parse_ineq(const std::string &str) {
   return ineq_();
 }
 */
+
+int Parser::id_() {
+  // Parse zero
+  if (*p_ == '0') {
+    p_++;
+    return 0;
+  }
+
+  // Parse optional negative sign
+  bool negate = (*p_ == '-');
+  if (negate) {
+    p_++;
+  }
+
+  if ('1' > *p_ || *p_ > '9') {
+    throw std::range_error("Leading zero not found");
+  }
+
+  int id = int(*p_) - int('0');
+  assert(1 <= id && id <= 9);
+  const int MAX_DIGITS = 8;
+  for (int i = 0; i < MAX_DIGITS; i++) {
+    // Pass if non-digit
+    if ('0' > *p_ || *p_ > '9') {
+      break;
+    }
+    id = 10 * id + (int(*p_) - int('0'));
+  }
+
+  return id;
+}
