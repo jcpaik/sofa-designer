9c3424de79dd66b4cf113d0629bf99c0d5de4ccb
Jineon Baek <jcpaik@Jineons-MacBook-Air.local>
2022-06-06 09:52:03 -0400

    Now sprove compilable

--- bin/parse.cc
+++ bin/parse.cc
@@ -2,12 +2,14 @@
 
 Parser::Parser(const SofaContext &ctx) : ctx(ctx) {}
 
+/*
 LinearForm Parser::parse_expr(const std::string &str) {
-  c_ = str.c_str();
+  p_ = str.c_str();
   return length_sum_();
 }
 
-LinearForm Parser::parse_ineq(const std::string &str) {
-  c_ = str.c_str();
+LinearInequality Parser::parse_ineq(const std::string &str) {
+  p_ = str.c_str();
   return ineq_();
 }
+*/
--- bin/sprove.cc
+++ bin/sprove.cc
@@ -13,63 +13,17 @@ namespace po = boost::program_options;
 #include "sofa/geom.h"
 #include "sofa/branch_tree.h"
 #include "sofa/cereal.h"
-#include "csv.h"
-
-static bool endsWith(const std::string& str, const std::string& suffix) {
-    return str.size() >= suffix.size() && 0 == 
-      str.compare(str.size()-suffix.size(), suffix.size(), suffix);
-}
+#include "parse.h"
 
 int main(int argc, char* argv[]) {
-  try {
-    std::string name;
-    unsigned int nthreads;
+  SofaContext ctx({
+      {QT{12,13},QT{5,13}},
+      {QT{4,5},QT{3,5}},
+      {QT{3,5},QT{4,5}},
+      {QT{5,13},QT{12,13}}
+      });
 
-    // Set up syntax for arguments
-    po::options_description desc("Allowed options");
-    desc.add_options()
-      ("help", "Produce help message")
-      ("name", po::value<std::string>(&name), "The name of .sofas file to view")
-      ("nthreads", po::value<unsigned int>(&nthreads)->implicit_value(1),
-         "Number of threads to use (optional)\n"
-         "Note that the output is not deterministic "
-         "when the option is specified")
-      ;
-
-    po::positional_options_description p;
-    p.add("name", -1);
-
-    // Parse arguments
-    po::variables_map vm;        
-    po::store(po::command_line_parser(argc, argv).
-              options(desc).positional(p).run(), vm);
-    po::notify(vm);    
-
-    // Logic
-    if (vm.count("help")) {
-      std::cout << desc << "\n";
-      return 0;
-    }
-
-    const std::string ext(".angles");
-    if (!vm.count("angles")) {
-      throw std::invalid_argument("Angles missing");
-    } else if (!endsWith(angles, ext)) {
-      throw std::invalid_argument(
-          "The file for angles should have .angles extension\n");
-    }
-
-    std::string name = angles.substr(0, angles.size() - ext.size());
-    std::ifstream inp(angles);
-    auto gamma_csv = readCSV(inp);
-    process_angles(gamma_csv, name);
-  } catch(std::exception& e) {
-    std::cerr << "error: " << e.what() << "\n";
-    return 1;
-  } catch(...) {
-    std::cerr << "Exception of unknown type!\n";
-    return 1;
-  }
+  Parser p(ctx);
 
   return 0;
 }
