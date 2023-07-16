6b718afb67e339657a715d7cd135c3d566cb84ac
Jineon Baek <jineon@umich.edu>
2023-07-16 16:11:37 +0900

    lift unnecessary assert + test

---
+++ lib/sofa/json.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+#include <json/json.h>
+
+#include "expect.h"
+#include "number.h"
+
+inline Json::Value to_json(const NT &n) {
+  return n.get_str();
+}
+
+inline Json::Value to_json(const QT &q) {
+  return q.numerator().get_str() + "/" + q.denominator().get_str();
+}
+
+template <typename T>
+inline Json::Value to_json(const std::vector<T> &vec) {
+  Json::Value arr = Json::arrayValue;
+  for (const auto &val : vec)
+    arr.append(to_json(val));
+  return arr;
+}
+
+inline QT stoq(const std::string &str) {
+  std::istringstream buf(str);
+  QT ret;
+  buf >> ret;
+  expect(buf.eof());
+  return ret;
+}
+
+inline QT qt_from_json(const Json::Value val) {
+  expect(val.isString());
+  return stoq(val.asString());
+}
+
+inline std::vector<QT> qts_from_json(Json::Value arr) {
+  expect(arr.type() == Json::arrayValue);
+  size_t n = arr.size();
+  std::vector<QT> ret(n);
+  for (int i = 0; i < n; i++) {
+    ret[i] = qt_from_json(arr[i]);
+  }
+  return ret;
+}
---
+++ test/split_values.cc
@@ -0,0 +1,47 @@
+#include "catch.hpp"
+
+#include <algorithm>
+#include <iostream>
+
+#include "sofa/number.h"
+#include "sofa/context.h"
+#include "sofa/qp.h"
+#include "sofa/branch_tree.h"
+#include "sofa/json.h"
+
+LinearForm linearForm(Json::Value val) {
+  return LinearForm(qt_from_json(val["b"]), qts_from_json(val["a"]));
+}
+
+TEST_CASE( "JSON certificate export of splitting values", "[CERTIFICATE]" ) {
+  SofaContext ctx( {
+      {QT{2496,2545}, QT{497,2545}}, 
+      {QT{12,13}, QT{5,13}}, {QT{3596,4325}, QT{2403,4325}},
+      {QT{803761,1136689}, QT{803760,1136689}}, 
+      {QT{2403,4325}, QT{3596,4325}}, 
+      {QT{5,13}, QT{12,13}}, {QT{497,2545}, QT{2496,2545}}
+      }
+      );
+
+  int n = ctx.n();
+  int idx = 1;
+
+  Json::Value vals = ctx.split_values();
+  REQUIRE(vals[0].isNull());
+
+  for (int i = 0; i <= 2 * n; i++)
+    REQUIRE(linearForm(vals[idx++]) == dot(ctx.A(i + 1) - ctx.A(i), ctx.v(i)));
+  
+  for (int l = 1; l <= (n - 1); l++)
+    for (int j = -(n - 1); j <= (n - 1); j++)
+      for (int i = -(n - 1); i < j; i++) {
+        REQUIRE(linearForm(vals[idx++]) == ctx.p(l - n, l).x - ctx.p(i, j).x);
+      }
+  
+  for (int k = -(n - 1); k <= (n - 1); k++)
+    for (int j = -(n - 1); j < k; j++)
+      for (int i = -(n - 1); i < j; i++) {
+        REQUIRE(linearForm(vals[idx++]) == dot(ctx.p(i, j), ctx.v(k)) -
+          ctx.s(k + n) + LinearForm::constant(ctx.d(), 1));
+      }
+}
