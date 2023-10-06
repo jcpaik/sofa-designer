bc4d323350e54a5136cbedc80d9bfa8c30c71d8f
Jineon Baek <jcpaik@jcpaik-desktop.local>
2023-10-06 10:35:32 -0400

    multithreading

--- bin/sprove.cc
+++ bin/sprove.cc
@@ -9,6 +9,9 @@ namespace po = boost::program_options;
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <shared_mutex>
+#include <thread>
+#include <future>
 
 #include "sofa/context.h"
 #include "sofa/geom.h"
@@ -17,23 +20,6 @@ namespace po = boost::program_options;
 #include "sofa/cereal.h"
 #include "parse.h"
 #include "tqdm.h"
-
-/*
-  SofaContext ctx({
-      {QT{12,13},QT{5,13}},
-      {QT{4,5},QT{3,5}},
-      {QT{3,5},QT{4,5}},
-      {QT{5,13},QT{12,13}}
-      });
-
-  Parser p(ctx);
-  auto res = p.parse_expr("dot(A(0)-A(5),u(0))");
-  assert (res == dot(ctx.u(0), ctx.A(0) - ctx.A(ctx.n())));
-  for (int i = 1; i < ctx.n(); i++) {
-    auto res = p.parse_expr(std::string("dot(A(0)-p(0,-")+std::to_string(i)+"),u(0))");
-    assert (res == dot(ctx.u(1), ctx.A(0) - ctx.p(0, i)));
-  }
-*/
 
 bool report_incompatible(
     const SofaState &v,
@@ -47,38 +33,73 @@ bool report_incompatible(
   }
 }
 
+std::shared_mutex mutex;
+QT lb, lb_min, lb_max;
+int bsearch_depth = 5;
+
+void find_lb_worker(
+    int worker_id, int num_workers,
+    const std::vector<SofaState> &nodes, 
+    const LinearForm &val) {
+  tqdm *bar = worker_id ? nullptr : new tqdm();
+  int c = 0, n = (int(nodes.size()) - 1) / num_workers + 1;
+  for (int i = worker_id; i < nodes.size(); i += num_workers) {
+    const auto &v = nodes[i];
+    if (bar)
+      bar->progress(c++, n);
+
+    mutex.lock_shared();
+    if (bar)
+      bar->set_label(to_json(lb).asString());
+    
+    if (!report_incompatible(v, val, lb)) {
+      mutex.unlock_shared();
+      mutex.lock();
+      if (!report_incompatible(v, val, lb)) {
+        bool lb_found = false;
+        QT clb = lb_min, cub = lb_max;
+        for (int i = 0; i < bsearch_depth; i++) {
+          QT md = (clb + cub) / 2;
+          if (report_incompatible(v, val, md))
+            lb_found = true, clb = md;
+          else
+            cub = md;
+        }
+        if (!lb_found)
+          throw std::runtime_error("Lower bound invalid");
+        lb = clb;
+      }
+      mutex.unlock();
+    }
+    else
+      mutex.unlock_shared();
+  }
+  if (bar)
+    bar->finish();
+}
+
 void find_lb(
     const SofaBranchTree &tree, 
     const LinearForm &val,
-    QT lb, QT ub, int bsearch_depth,
-    const std::string &out) {
+    QT min, QT max,
+    const std::string &out,
+    int nthread) {
+  lb_min = min;
+  lb = lb_max = max;
 
-  QT res = ub;
-  tqdm bar;
-  int c = 0, n = tree.valid_states().size();
-  for (const auto &v : tree.valid_states()) {
-    bar.progress(c++, n);
-    bar.set_label(to_json(res).asString());
-    
-    if (!report_incompatible(v, val, res)) {
-      QT clb = lb, cub = ub;
-      for (int i = 0; i < bsearch_depth; i++) {
-        QT md = (clb + cub) / 2;
-        if (report_incompatible(v, val, md))
-          clb = md;
-        else
-          cub = md;
-      }
-      res = clb;
-      expect(report_incompatible(v, val, res));
-    }
-  }
-  bar.finish();
-  std::cout << to_json(res).asString() << std::endl;
+  const auto &nodes = tree.valid_states();
+
+  std::future<void> futures[nthread];
+  for (int i = 0; i < nthread; i++)
+    futures[i] = std::async(find_lb_worker, i, nthread, nodes, val);
+  for (auto &f : futures)
+    f.get();
+  
+  std::cout << to_json(lb).asString() << std::endl;
 }
 
 void run(const std::string &treefile, const std::string &ineq, 
-    QT lb, QT ub, const std::string &out) {
+    QT lb, QT ub, const std::string &out, int nthreads) {
 
   CerealReader reader(treefile.c_str());
   SofaContext ctx(reader);
@@ -89,7 +110,7 @@ void run(const std::string &treefile, const std::string &ineq,
   Parser parser(ctx);
   LinearForm val = parser.parse_expr(ineq);
 
-  find_lb(tree, val, lb, ub, 5, out);
+  find_lb(tree, val, lb, ub, out, nthreads);
 
   // TODO: json output functionality
 }
@@ -97,6 +118,7 @@ void run(const std::string &treefile, const std::string &ineq,
 int main(int argc, char* argv[]) {
   try {
     std::string tree, ineq, lb_str, ub_str, out;
+    int nthreads;
 
     // Set up syntax for arguments
     po::options_description desc("Allowed options");
@@ -106,7 +128,9 @@ int main(int argc, char* argv[]) {
       ("ineq", po::value<std::string>(&ineq), "Required inequality in string form")
       ("lb", po::value<std::string>(&lb_str), "Required inequality in string form")
       ("ub", po::value<std::string>(&ub_str), "Required inequality in string form")
-      ("out", po::value<std::string>(&out)->implicit_value(""),
+      ("out", po::value<std::string>(&out)->default_value(""),
+         "Directory for proof output in json (optional)\n")
+      ("nthreads", po::value<int>(&nthreads)->default_value(1),
          "Directory for proof output in json (optional)\n")
       ;
 
@@ -120,7 +144,7 @@ int main(int argc, char* argv[]) {
     po::variables_map vm;        
     po::store(po::command_line_parser(argc, argv).
               options(desc).positional(p).run(), vm);
-    po::notify(vm);    
+    po::notify(vm);
 
     // Logic
     if (vm.count("help")) {
@@ -128,7 +152,7 @@ int main(int argc, char* argv[]) {
       return 0;
     }
 
-    run(tree, ineq, stoq(lb_str), stoq(ub_str), out);
+    run(tree, ineq, stoq(lb_str), stoq(ub_str), out, nthreads);
   } catch(std::exception& e) {
     std::cerr << "error: " << e.what() << "\n";
     return 1;
